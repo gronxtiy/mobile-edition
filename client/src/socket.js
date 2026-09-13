61bebea0a4fb520bbd89e12dc0fab8ddc38cdaa6
gronxtiy <nikhilkota@gronxtiy.com>
2026-09-13 22:56:54 +0530

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -8,11 +8,24 @@ const socket = io(import.meta.env.VITE_API_URL, {
 export default socket;*/}
 
 
-import { io } from "socket.io-client";
+/*import { io } from "socket.io-client";
 
 const socket = io(import.meta.env.VITE_API_URL, {
   withCredentials: true,
   transports: ["websocket", "polling"],
+});
+
+export default socket;*/
+
+
+
+
+import { io } from "socket.io-client";
+
+const socket = io(import.meta.env.VITE_API_URL, {
+  withCredentials: true,
+  transports: ["polling", "websocket"],
+  autoConnect: true,
 });
 
 export default socket;
