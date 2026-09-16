108fb4a92bdafd0e2fa249b56a2c1e4188c6135d
gronxtiy <nikhilkota@gronxtiy.com>
2026-09-16 20:30:00 +0530

    Update ProtectedRoute.jsx

--- client/src/componets/ProtectedRoute.jsx
+++ client/src/componets/ProtectedRoute.jsx
@@ -3,36 +3,29 @@ import { Navigate } from "react-router-dom";
 import axios from "axios";
 
 const ProtectedRoute = ({ children, allowedRole }) => {
+
   const [loading, setLoading] = useState(true);
   const [authorized, setAuthorized] = useState(false);
 
   useEffect(() => {
-    axios
-      .get(`${import.meta.env.VITE_API_URL}/api/auth/me`, {
-        withCredentials: true,
-      })
-      .then((res) => {
-        if (res.data.role === allowedRole) {
-          setAuthorized(true);
-        } else {
-          setAuthorized(false);
-        }
-      })
-      .catch((err) => {
-        console.log(
-          "Auth error:",
-          err.response?.data || err.message
-        );
-        setAuthorized(false);
-      })
-      .finally(() => {
-        setLoading(false);
-      });
+
+    axios.get(`${import.meta.env.VITE_API_URL}/api/${allowedRole}/dashboard`, {
+      withCredentials: true
+    })
+    .then(() => {
+      setAuthorized(true);
+    })
+    .catch((err) => {
+      console.log("Auth error:", err.response?.data || err.message);
+      setAuthorized(false);
+    })
+    .finally(() => {
+      setLoading(false);
+    });
+
   }, [allowedRole]);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+  if (loading) return <p>Loading...</p>;
 
   if (!authorized) {
     return <Navigate to="/login" replace />;
