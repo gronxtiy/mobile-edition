9d9d32a96d9808aef08576accbc73921d7f8d793
gronxtiy <nikhilkota@gronxtiy.com>
2026-09-23 22:23:00 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,22 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
+// Send cookies with Axios requests
+axios.defaults.withCredentials = true;
+
+// Automatically attach JWT token to Axios requests
+axios.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
